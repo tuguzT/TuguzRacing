2178b2624e31cf8b3a6ef6bcee040c09d0d06c9c
tuguzT <timurka.tugushev@gmail.com>
2021-02-21 21:52:12 +0300

    C++ project associated with UE4 project was added

---
+++ Source/TuguzRacing/WheeledVehiclePawn.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WheeledVehicle.h"
+#include "WheeledVehiclePawn.generated.h"
+
+/**
+ * Base class of wheeled vehicle pawn
+ */
+UCLASS()
+class TUGUZRACING_API AWheeledVehiclePawn : public AWheeledVehicle
+{
+	GENERATED_BODY()
+	
+};
